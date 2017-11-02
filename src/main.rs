8c8feebd70d6073a64130bebdd27c54489725346
Vzaa <Vzaa@users.noreply.github.com>
2017-11-02 14:44:05 +0300

    Make stuff generic

--- src/main.rs
+++ src/main.rs
@@ -13,9 +13,9 @@ use image::{ImageBuffer, Rgb};
 pub type Pix = Rgb<u8>;
 pub type ImgRgb = ImageBuffer<Pix, Vec<u8>>;
 
-fn dist<T>(a: &[T], b: &[T]) -> f32
+fn euclid<T>(a: &[T], b: &[T]) -> f32
 where
-    f32: From<T>,
+    T: Into<f32>,
     T: Copy,
 {
     a.iter().zip(b.iter()).fold(0.0_f32, |acc, (x, y)| {
@@ -27,43 +27,61 @@ where
     })
 }
 
-
-pub struct KMeans<T>
-where
-    T: Sized,
-    T: Copy,
-    T: rand::Rand,
-{
-    vals: Vec<[T; 3]>,
+pub trait AsSlice {
+    type Item;
+    fn as_slice(&self) -> &[Self::Item];
 }
 
+pub trait Dist {
+    fn dist(&self, other: &Self) -> f32;
+}
+
+impl<T, U> Dist for T
+where
+    T: AsSlice<Item = U>,
+    U: Into<f32>,
+    U: Copy,
+{
+    fn dist(&self, other: &Self) -> f32 {
+        euclid(self.as_slice(), other.as_slice())
+    }
+}
+
+impl AsSlice for Pix {
+    type Item = u8;
+    fn as_slice(&self) -> &[u8] {
+        &self.data
+    }
+}
+
+pub struct KMeans<T=Sized> {
+    vals: Vec<T>,
+}
 
 impl<T> KMeans<T>
 where
-    T: Copy,
-    f32: From<T>,
-    T: rand::Rand,
+    T: Dist,
 {
-    pub fn new(k: usize) -> KMeans<T> {
-        let mut rng = thread_rng();
-        let mut vals = Vec::new();
+    //pub fn new(k: usize) -> KMeans<T> {
+    //let mut rng = thread_rng();
+    //let mut vals = Vec::new();
 
-        for _ in 0..k {
-            vals.push([rng.gen(), rng.gen(), rng.gen()]);
-        }
+    //for _ in 0..k {
+    //vals.push(rng.gen());
+    //}
 
-        KMeans { vals }
-    }
+    //KMeans { vals }
+    //}
 
-    pub fn class_val(&self, p: &[T]) -> &[T; 3] {
+    pub fn class_val(&self, p: &T) -> &T {
         let idx = self.class_idx(p);
         &self.vals[idx]
     }
 
-    pub fn class_idx(&self, p: &[T]) -> usize {
+    pub fn class_idx(&self, p: &T) -> usize {
         let m = self.vals
             .iter()
-            .map(|k| dist(k, p))
+            .map(|k| k.dist(p))
             .enumerate()
             .min_by(|a, b| a.1.partial_cmp(&b.1).unwrap());
 
@@ -71,28 +89,41 @@ where
     }
 }
 
-impl KMeans<u8> {
+impl KMeans<Pix> {
+    pub fn newc(k: usize) -> KMeans<Pix> {
+        let mut rng = thread_rng();
+        let mut vals = Vec::new();
+
+        for _ in 0..k {
+            vals.push(Pix { data: rng.gen() });
+        }
+
+        KMeans { vals }
+    }
+
     pub fn update(&mut self, imgrgb: &ImgRgb) {
         let new_centers: Vec<_> = (0..self.vals.len())
             .into_par_iter()
             .map(|c| {
                 let (cnt, sums) = imgrgb
                     .pixels()
-                    .filter(|&p| self.class_idx(&p.data) == c)
-                    .map(|p| [p.data[0] as u64, p.data[1] as u64, p.data[2] as u64])
-                    .fold((0, [0, 0, 0]), |(cnt, acc), x| {
-                        (cnt + 1, [acc[0] + x[0], acc[1] + x[1], acc[2] + x[2]])
+                    .filter(|&p| self.class_idx(p) == c)
+                    .map(|p| p.as_slice())
+                    .fold((0, [0_u64, 0_u64, 0_u64]), |(cnt, mut acc), x| {
+                        (cnt + 1, { acc.iter_mut().zip(x.iter()).for_each(|(a, b)| *a += *b as u64); acc })
                     });
 
                 if cnt == 0 {
                     return None;
                 }
 
-                Some([
-                    (sums[0] / cnt) as u8,
-                    (sums[1] / cnt) as u8,
-                    (sums[2] / cnt) as u8,
-                ])
+                Some(Pix {
+                    data: [
+                        (sums[0] / cnt) as u8,
+                        (sums[1] / cnt) as u8,
+                        (sums[2] / cnt) as u8,
+                    ],
+                })
             })
             .collect();
 
@@ -118,10 +149,12 @@ fn main() {
         .parse()
         .unwrap();
 
+    let outfl = env::args().nth(4).unwrap_or("qout.png".to_owned());
+
     let mut img = image::open(&filename).unwrap();
 
     {
-        let mut kmeans = KMeans::new(k);
+        let mut kmeans = KMeans::newc(k);
         let imgrgb = img.as_mut_rgb8().expect("Cannot read image as RGB");
 
         // Iterate a fixed amount
@@ -131,11 +164,10 @@ fn main() {
 
         // Quantize the image
         for p in imgrgb.pixels_mut() {
-            let v = kmeans.class_val(&p.data);
-            *p = Pix { data: *v };
+            *p = *kmeans.class_val(p);
         }
     }
 
-    let mut fout = File::create("qout.png").unwrap();
+    let mut fout = File::create(outfl).unwrap();
     img.save(&mut fout, image::PNG).unwrap();
 }
